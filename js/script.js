e11161f25e490dc75c3fa95d21b5ac1f8b2e02e4
andriyoanggo <andriyo.anggo@gmail.com>
2024-09-13 21:51:20 +0700

    hide-main-content-DOM

--- js/script.js
+++ js/script.js
@@ -6,15 +6,20 @@
 //    let inisiasi = 10;
 
 //    if (inisiasi == 1) {
-        console.log('Benar');
+//        console.log('Benar');
 //    } else if (inisiasi == 10) {
-            console.log('Benar');
+//            console.log('Benar');
 //    } else {
 //        console.log('Salah');
 //    }
 //}
+//function tryDOM() {
+//    let getDOM = document.getElementById('jajar-genjang-btn');
+//    getDOM.style.backgroundColor = 'brown';
+//    console.log(getDOM);
+
 function tryDOM() {
-    let getDOM = document.getElementById('jajar-genjang-btn');
-    getDOM.style.backgroundColor = 'brown';
+    let getDOM = document.getElementsByClassName('main-content');
+    getDOM[0].style.display = 'none';
     console.log(getDOM);
 }
