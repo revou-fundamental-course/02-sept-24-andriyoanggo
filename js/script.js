07f0d6ee7ff6376ed23de1a6ce3fdcf85b531287
andriyoanggo <andriyo.anggo@gmail.com>
2024-09-13 20:45:50 +0700

    inisiasi

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 // Ini javascript
+let inisiasi = 10;
+
+if (inisiasi == 1) {
+    console.log('Benar');
+} else if (inisiasi == 10) {
+        console.log('Benar');
+} else {
+    console.log('Salah');
+}
